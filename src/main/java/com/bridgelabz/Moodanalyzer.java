9429c40f01ac6ed4b573b2938df33c06adc2b891
bhimineni-kalyani <bhiminenikalyani@gmail.com>
2021-03-15 17:05:25 +1400

    UC1

--- src/main/java/com/bridgelabz/Moodanalyzer.java
+++ src/main/java/com/bridgelabz/Moodanalyzer.java
@@ -1,4 +1,18 @@
 package com.bridgelabz;
 
 public class Moodanalyzer {
+    private String message;
+
+    public Moodanalyzer(String message) {
+        this.message=message;
+    }
+
+    public String analyzeMood() {
+        if(message.contains("sad")) {
+            return "Sad";
+        }
+        else {
+            return "Happy";
+        }
+    }
 }
--- src/test/java/com/bridgelabz/Moodanalyzertest.java
+++ src/test/java/com/bridgelabz/Moodanalyzertest.java
@@ -1,5 +1,24 @@
 package com.bridgelabz;
 
-public class Moodanalyzertest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class Moodanalyzertest {
+    private Assertions Assert;
+
+    @Test
+    public void givenMessageReturnSad() {
+        String message = "I am in sad Mood";
+        Moodanalyzer mood=new Moodanalyzer(message);
+        String message1= mood.analyzeMood();
+        Assertions.assertEquals("Sad",message1);
+    }
+
+    @Test
+    public void giveMessageReturnHappy() {
+        String message = "I am in any Mood";
+        Moodanalyzer mood=new Moodanalyzer(message);
+        String message1= mood.analyzeMood();
+        Assert.assertEquals("Happy",message1);
+    }
 }
